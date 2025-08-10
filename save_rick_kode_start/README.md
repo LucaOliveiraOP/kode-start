bfe54ba013620ace3d66d26a29a6476f457b6e9d
Luca Aguiar Oliveira Paiva <luca.oliveira@institutoiracema.org.br>
2025-08-10 10:46:41 -0300

    add empty project

---
+++ save_rick_kode_start/README.md
@@ -0,0 +1,3 @@
+# save_rick_kode_start
+
+A new Flutter project.
---
+++ save_rick_kode_start/android/app/src/main/kotlin/com/example/save_rick_kode_start/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.save_rick_kode_start
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
