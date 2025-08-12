065254871a1ccf742a242c65c0ac2592889eb3f0
Luca Aguiar Oliveira Paiva <luca.oliveira@institutoiracema.org.br>
2025-08-12 01:13:10 -0300

    style: add new font, images and sounds.

--- save_rick_kode_start/lib/theme/app_colors.dart
+++ save_rick_kode_start/lib/theme/app_colors.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static Color primaryColorDark = const Color(0xff5D78F0);
-  static Color primaryColorLight = const Color(0xff87A1FA);
+  static Color primaryColorDark = const Color(0xFF567BCC);
+  static Color primaryColorLight = const Color(0xFF82E6EB);
   static Color appBarColor = const Color(0xff1C1B1F);
   static Color backgroundColor = Colors.black;
   static Color white = Colors.white;
--- save_rick_kode_start/lib/theme/app_images.dart
+++ save_rick_kode_start/lib/theme/app_images.dart
@@ -1,5 +1,8 @@
 class AppImages {
   AppImages._();
 
-  static const logo = 'assets/logo.png';
+  static const logo = 'assets/images/logo.svg';
+  static const logorick = 'assets/images/rick_and_morty_logo.svg';
+  static const pickle = 'assets/images/picke_rick.png';
+  static const portal = 'assets/images/portal-rick-and-morty.gif';
 }
